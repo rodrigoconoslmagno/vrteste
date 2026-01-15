e810afc819ae3e4aaaec93c5a3a025f8acd5d4de
Rodrigo Consolmagno <rodrigoconsolmagno@hotmail.com>
2026-01-15 12:47:35 -0300

    Inicializacao unica

--- src/main/java/br/com/magno/pedido/PedidoApplication.java
+++ src/main/java/br/com/magno/pedido/PedidoApplication.java
@@ -1,13 +1,23 @@
 package br.com.magno.pedido;
 
-import org.springframework.boot.SpringApplication;
+import java.awt.EventQueue;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import br.com.magno.pedido.view.PedidoForm;
 
 @SpringBootApplication
 public class PedidoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PedidoApplication.class, args);
+		new SpringApplicationBuilder(PedidoApplication.class)
+                .headless(false)
+                .run(args);
+
+        // Abre a tela Swing
+        EventQueue.invokeLater(() -> {
+            PedidoForm form = new PedidoForm();
+            form.setVisible(true);
+        });
 	}
 
 }
